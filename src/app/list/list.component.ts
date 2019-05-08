60a2c381a9bca9b43e7f0b240dfc12c0888c051a
massimo <massipetra@hotmail.it>
2019-05-08 12:59:15 +0200

    Event interface created

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Event } from '../event';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+  events: Event[];
   data = [
     'Racing car sprays burning fuel into crowd.',
     'Japanese princess to wed commoner.',
